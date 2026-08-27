0d43da3783156fb24571e004e01664a8e141f206
Abraham <165726674+Abraham2106@users.noreply.github.com>
2026-08-26 19:16:25 -0600

    feat(class05): register class 05 route

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Class01 from './pages/Class01'
 import Class02 from './pages/Class02'
 import Class03 from './pages/Class03'
 import Class04 from './pages/Class04'
+import Class05 from './pages/Class05'
 import MarkdownPage from './pages/MarkdownPage'
 import NotFound from './pages/NotFound'
 
@@ -19,6 +20,7 @@ function AppRoutes() {
         <Route path="class/02" element={<Class02 />} />
         <Route path="class/03" element={<Class03 />} />
         <Route path="class/04" element={<Class04 />} />
+        <Route path="class/05" element={<Class05 />} />
         <Route path="markdown/*" element={<MarkdownPage />} />
         <Route path="*" element={<NotFound />} />
       </Route>
